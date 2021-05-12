b111fd295ab0561303a150812ef61f991e8cb7af
Wilmer Prieto <wilmer.prieto@hunatec.com>
2021-05-11 22:11:34 -0400

    Toggle component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Toogle from './components/Toggle';
 
-const App = () => (
-  <div className="App">
-    <Toogle />
-  </div>
-);
+const App = () => {
+  const [isChecked, setIsChecked] = useState(false);
+
+  const handleChange = value => {
+    setIsChecked(value);
+  };
+
+  return (
+    <div className="App">
+      <Toogle isChecked={isChecked} handleChange={handleChange} />
+    </div>
+  );
+};
 
 export default App;
